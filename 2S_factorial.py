615261dfa90f2c2e0cfb0045b9067165fa2c9993
istone1000 <116089208+istone1000@users.noreply.github.com>
2022-11-24 10:13:53 +0000

    Update 2S_factorial.py

--- 2S_factorial.py
+++ 2S_factorial.py
@@ -3,7 +3,7 @@
 #Factorial
 
 def fact(n):             #n is the parameter
-    if n == 0:
+    if n == 1:
         return 1
     return n * fact (n-1)                          #ask what do we want n multiplied by? one less than it?
 
